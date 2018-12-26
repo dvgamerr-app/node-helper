31fb48a750b6054110f298575c206a560c7199ce
Kananek Thongkam <info.dvgamer@gmail.com>
2018-12-26 12:27:28 +0700

    project init

---
+++ test/index.js
@@ -0,0 +1,44 @@
+const { expect, assert } = require('chai')
+
+describe('node_debuger', () => {
+  const main = require('../index')
+
+  // describe('#helper()', () => {
+  //   // it('Functional -- raven', done => {
+  //   //   const Raven = require('../helper/raven')
+  //   //   Raven.install({}, 'unit-testing')
+  //   //   Raven.warning('test message')
+  //   //   Raven.error(new Error('Testing Error.'))
+  //   //   Raven.ProcessClosed(process, async () => {})
+  //   //   Raven.Tracking(async () => {
+  //   //     done()
+  //   //   })
+  //   // })
+  //   it('class time.seconds', done => {
+  //     let elapsed = new main.Time()
+  //     assert(typeof elapsed.seconds, 'function')
+  //   })
+  //   it('class time.nanoseconds', done => {
+  //     let elapsed = new main.Time()
+  //     expect(elapsed).to.have.property('nanoseconds')
+  //   })
+  //   it('class time.total', done => {
+  //     let elapsed = new main.Time()
+  //     expect(elapsed.seconds).to.have.property('total')
+  //   })
+  //   it('const DevMode', done => {
+  //     expect(main).to.have.property('DevMode')
+  //   })
+  //   it('const DebugMode', done => {
+  //     expect(main).to.have.property('DebugMode')
+  //   })
+  //   it('const IsLinux', done => {
+  //     expect(main).to.have.property('IsLinux')
+  //   })
+  //   it('const IsWindows', done => {
+  //     expect(main).to.have.property('IsWindows')
+  //   })
+  // })
+  
+
+})
